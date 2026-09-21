0fdabc3ea71d9dda7694a4abb449b941fd1e6609
Phil Piwonka <wnka@users.noreply.github.com>
2026-09-20 17:10:36 -0700

    Update resume with Amazon experience

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ const browser = await puppeteer.launch()
 const page = await browser.newPage()
 
 await page.setContent(html, { waitUntil: 'networkidle0' })
+await page.addStyleTag({
+  content: `
+    @media print {
+      html { font-size: 12px; }
+      body { gap: 1em; margin-bottom: 2em; }
+      .masthead { padding: 2.5em 0; }
+      .stack { gap: 1em; }
+      #work .stack {
+        display: block;
+      }
+      #work .stack > article + article {
+        margin-top: 1em;
+      }
+    }
+  `
+})
 await page.pdf({ path: 'resume.pdf', format: 'letter', printBackground: true, margin: {top: 20, bottom: 20} })
 await browser.close()
